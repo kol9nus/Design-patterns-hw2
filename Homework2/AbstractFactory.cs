eac09b2aa6ef02fefe59ad44da103f7b32ff40e3
Nikolai Bravilov <bravilov@mail.ru>
2016-10-21 11:11:21 +0500

    Начальный коммит

---
+++ Homework2/AbstractFactory.cs
@@ -0,0 +1,85 @@
+﻿namespace Homework2
+{
+    public interface ICarFactory
+    {
+        ICarcass CreateCarcass();
+        ICabin CreateCabin();
+        IEngine CreateEngine();
+    }
+
+    public interface ICarcass
+    {
+        string Name { get; }
+    }
+
+    public interface IEngine
+    {
+        string Name { get; }
+    }
+
+    public interface ICabin
+    {
+        string Name { get; }
+    }
+
+    public class BMWFactory : ICarFactory
+    {
+        public ICarcass CreateCarcass()
+        {
+            return new PlasticCarcass();
+        }
+
+        public ICabin CreateCabin()
+        {
+            return new LeatherCabin();
+        }
+
+        public IEngine CreateEngine()
+        {
+            return new PowerfulEngline();
+        }
+    }
+
+    public class PlasticCarcass : ICarcass
+    {
+        public string Name => "Пластиковый кузов";
+    }
+
+    public class LeatherCabin : ICabin
+    {
+        public string Name => "Моднючий кожанный салон";
+    }
+
+    public class PowerfulEngline : IEngine
+    {
+        public string Name => "Зверский движок";
+    }
+
+    public class AudiFactory : ICarFactory
+    {
+        public ICarcass CreateCarcass()
+        {
+            return new PlasticCarcass();
+        }
+
+        public ICabin CreateCabin()
+        {
+            return new NormalCabin();
+        }
+
+        public IEngine CreateEngine()
+        {
+            return new NormalEngine();
+        }
+    }
+
+    public class NormalEngine : IEngine
+    {
+        public string Name => "Обычный двигатель";
+    }
+
+    public class NormalCabin : ICabin
+    {
+        public string Name => "Обычный салон";
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Комплектация ауди
+            ICarFactory audiFactory = new AudiFactory();
+            PrintResultingCar(audiFactory);
+
+            Console.WriteLine();
+
+            // Комплектация bmw
+            ICarFactory bmwFactory = new BMWFactory();
+            PrintResultingCar(bmwFactory);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintResultingCar(ICarFactory factory)
+        {
+            Console.WriteLine("Машина имеет: ");
+            var carcass = factory.CreateCarcass();
+            Console.WriteLine(carcass.Name);
+            var cabin = factory.CreateCabin();
+            Console.WriteLine(cabin.Name);
+            var engine = factory.CreateEngine();
+            Console.WriteLine(engine.Name);
+        }
+    }
+}
